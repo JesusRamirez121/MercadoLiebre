85c3588f7a0e4d9c552a8a33a74cd821b24b14f6
JesusRamirez121 <jesus1.ramirez@ucp.edu.co>
2022-02-22 18:47:31 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const publicPath = path.resolve(__dirname,'./public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.listen(8080, () =>
+   console.log('Servidor corriendo')
+);
+
+
